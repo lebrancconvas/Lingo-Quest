3a889e5bf5e5da2e9a351536944a0ece42980fe3
lebrancconvas <lebrancconvas@gmail.com>
2023-02-08 19:09:29 +0700

    ✨ feat: create select language page

---
+++ app/src/components/Card.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	export let language = "";
+</script>
+
+<main>
+	<div id="card">
+		{language}
+	</div>
+</main>
+
+<style>
+	#card {
+		border: 1px solid black; 
+		width: 30%;
+		height: 200px;  
+		margin: 10px auto; 
+		box-shadow: 1px 1px 1px 1px black;  
+		cursor: pointer; 
+	}
+
+</style>
--- app/src/routes/language_select/+page.svelte
+++ app/src/routes/language_select/+page.svelte
@@ -1,11 +1,31 @@
 <script lang="ts">
-
+	import Card from '../../components/Card.svelte';
+	const languageList = [
+		"Chinese",
+		"Japanese",
+		"Korean"
+	];
 </script>
 
 <div>
-
+	<header>
+		<div>
+			<h1>
+				Select Your Language
+			</h1>
+		</div>
+	</header>
+	<section id="language-option-section">
+		<div>
+			{#each languageList as language}
+				<Card language={language} />   
+			{/each}
+		</div>
+	</section>
 </div>
 
 <style>
-	
+	header {
+		text-align: center; 
+	}	
 </style>
