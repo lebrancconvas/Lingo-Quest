303a8ed41b9fa9484a5e5b40c44ff08624714936
lebrancconvas <lebrancconvas@gmail.com>
2023-02-08 19:09:10 +0700

    ✨ feat: fetching kanji data and render to page

---
+++ app/src/routes/vocabs/+page.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	// import { kanjiList } from '../../store/store.js';
+
+	let kanjiList: string[] = [];
+	const url = "https://kanjiapi.dev/v1/kanji/grade-1";
+
+	onMount(async() => {
+		fetch(url)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data); 
+				// kanjiList.set(data);
+				kanjiList = data; 
+			})
+			.catch(error => {
+				console.error(error);
+				console.log(`[ERROR] Fetching API Data error.`);
+			})
+	})
+</script>
+
+<main>
+	<div>
+		<ul>
+			{#each kanjiList as kanji}
+				<li>{kanji}</li> 
+			{/each}
+		</ul> 
+	</div>
+</main>
+
+<style>
+	li {
+		margin: 10px auto; 
+		font-size: 28px;
+		text-align: center; 
+	}
+</style>
