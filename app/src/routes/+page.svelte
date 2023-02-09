3f2feaf0071f56d4f6149060caae3810ea5273ab
lebrancconvas <lebrancconvas@gmail.com>
2023-02-09 17:08:51 +0700

    ✨ feat: create landing page

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -8,23 +8,50 @@
 <div>
 	<header>
 		<div>
-			<h1>Lingo Quest</h1>
+			<h1>Lingo Quest !</h1>
+		</div>
+		<div>
+			<h2>Learning Language can be fun</h2>
 		</div>
 	</header>
 	<section id="action-section">
-		<div>
-			<a href="/register">
-				<button>Register</button> 
-			</a>
-		</div>
-		<div>
-			<button>Login</button> 
-		</div>
+		<a href="/register">
+			<div id="join-button">
+				<h3>Join us !</h3> 
+			</div>
+		</a>
 	</section>
 </div>
 
 <style>
 	header {
 		text-align: center; 
+		color: white;
+	}
+
+	header > div > h1 {
+		font-size: 128px;
+	}
+
+	header > div > h2 {
+		font-size: 50px; 
+	}
+
+	#join-button {
+		width: 16%;
+		height: 25%;
+		border-radius: 10px; 
+		background-color: #edfc91; 
+		margin: 10px auto; 
+		cursor: pointer; 
+	}
+
+	#join-button > h3 {
+		text-align: center; 
+		color: #487bff; 
+	}
+
+	#join-button:active {
+		transform: scale(0.98); 
 	}
 </style>
