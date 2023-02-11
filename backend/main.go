d5ff9fee43eba20788c315d25b45b075e2ecd30f
lebrancconvas <lebrancconvas@gmail.com>
2023-02-11 16:47:02 +0700

    ✨ feat: demo api

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,45 @@
 package main
 
 import (
-
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-func main() {
+type User struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+	Displayname string `json:"displayname"`
+}
 
+func main() {
+	app := fiber.New();
+	
+	// Middleware
+
+	app.Use(cors.New());
+	app.Use(logger.New());
+	
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello Lingo Quest API.");
+	})
+
+	var users []User;
+
+	app.Post("/api/v1/users", func(c *fiber.Ctx) error {
+		newUser := User{
+			Username: "lebrancconvas",
+			Password: "asidw9qw",
+			Displayname: "Lebranc Convas",
+		}
+
+		users := append(users, newUser);
+		return c.JSON(users); 
+	})
+	
+	app.Get("/api/v1/users", func(c *fiber.Ctx) error {
+		return c.JSON(users); 
+	})
+
+	app.Listen(":8009");
 }
